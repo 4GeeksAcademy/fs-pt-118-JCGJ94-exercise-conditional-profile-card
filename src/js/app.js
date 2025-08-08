a7388ca22754ae583912863ee45a00870ef99b19
Jose C Gonzalez <jcgonj1994@gmail.com>
2025-08-08 18:50:40 +0000

    feat: conditional ok

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import { right } from "@popperjs/core";
 import "../style/index.css";
 
 /**
@@ -33,14 +34,20 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name ? variables.name : "name"} ${
+    variables.lastName ? variables.lastName : "lastName"
+  }</h1>
+          <h2>${variables.role ? variables.role : "Role"}</h2>
+          <h3>${variables.country || "country"},${variables.city || "city"}</h3>
+          <ul class=${variables.socialMediaPosition || "right"}>
+            <li><a href="https://twitter.com/${variables.twitter ||
+              "JC_GONJ94"}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github ||
+              "JCGJ94"}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${variables.linkedin ||
+              "jcgj94"}"><i class="fab fa-linkedin"></i></a></li>
+              <li><a href="https://instagram.com/${variables.instagram ||
+                "jc_gonj94"}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
